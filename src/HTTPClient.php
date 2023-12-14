02b44ec73f947744677a4542ce69d168d9248da6
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-14 04:55:45 +0000

    update

--- src/HTTPClient.php
+++ src/HTTPClient.php
@@ -4,16 +4,6 @@ namespace RPurinton\GeminiPHP;
 
 class HTTPClient
 {
-    public static function get($url, $headers = [])
-    {
-        return file_get_contents($url, false, stream_context_create([
-            'http' => [
-                'method' => 'GET',
-                'header' => $headers
-            ]
-        ]));
-    }
-
     public static function post($url, $data, $headers = [])
     {
         return file_get_contents($url, false, stream_context_create([
