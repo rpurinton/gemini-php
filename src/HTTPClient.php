8843a9084ee03cf94d4c5b7bcfa6044b4738b3bd
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-14 15:55:06 +0000

    update

--- src/HTTPClient.php
+++ src/HTTPClient.php
@@ -4,14 +4,14 @@ namespace RPurinton\GeminiPHP;
 
 class HTTPClient
 {
-    public static function post($url, $headers, $data)
+    public static function post(string $url, array $headers = [], string $data = ''): string
     {
-        return file_get_contents($url, false, stream_context_create([
+        return @file_get_contents($url, false, stream_context_create([
             'http' => [
                 'method' => 'POST',
                 'header' => $headers,
                 'content' => $data
             ]
-        ]));
+        ])) or throw new \Exception('Error: Unable to connect to Gemini API.');
     }
 }
