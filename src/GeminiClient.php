012b1c88952ba59a53abf60d673ffe269b312cfd
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-17 06:14:27 +0000

    update

--- src/GeminiClient.php
+++ src/GeminiClient.php
@@ -23,36 +23,29 @@ class GeminiClient
         $this->refreshAccessToken();
     }
 
-    /*
     public function refreshAccessToken(): void
     {
         if (time() > $this->expiresAt) {
-            $gcloud = trim(shell_exec('which gcloud') ?? '');
-            if (empty($gcloud)) throw new \Exception('Error: gcloud not found.');
-            if (!file_exists($this->credentialsPath)) throw new \Exception('Error: credentials file not found.');
-            $credentials = json_decode(file_get_contents($this->credentialsPath), true);
-            if (!$credentials) throw new \Exception('Error: credentials file JSON error.');
-            $cmd = 'export GOOGLE_APPLICATION_CREDENTIALS=' . $this->credentialsPath . ' && ' . $gcloud . ' auth application-default print-access-token';
-            $this->accessToken = trim(shell_exec($cmd) ?? '');
+            $this->validateCredentials();
+            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $this->credentialsPath);
+            $accessToken = ApplicationDefaultCredentials::getCredentials('https://www.googleapis.com/auth/cloud-platform')->fetchAuthToken();
+            if (!isset($accessToken['access_token'])) throw new \Exception('Error: Unable to get access token.');
+            $this->accessToken = $accessToken['access_token'];
             $this->expiresAt = time() + self::VALID_TIME;
-            if (empty($this->accessToken)) throw new \Exception('Error: Unable to get access token.');
         }
     }
-    */
 
-    public function refreshAccessToken(): void
+    public function validateCredentials(): void
     {
-        if (time() > $this->expiresAt) {
-            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $this->credentialsPath);
-            $auth = ApplicationDefaultCredentials::getCredentials('https://www.googleapis.com/auth/cloud-platform');
-            $accessToken = $auth->fetchAuthToken();
-            if (isset($accessToken['access_token'])) {
-                $this->accessToken = $accessToken['access_token'];
-                $this->expiresAt = time() + self::VALID_TIME;
-            } else {
-                throw new \Exception('Error: Unable to get access token.');
-            }
-        }
+        if (!file_exists($this->credentialsPath)) throw new \Exception('Error: Credentials file not found.');
+        if (!is_readable($this->credentialsPath)) throw new \Exception('Error: Credentials file not readable.');
+        $contents = file_get_contents($this->credentialsPath) or throw new \Exception('Error: Unable to read credentials file.');
+        $json = json_decode($contents, true) or throw new \Exception('Error: Unable to parse credentials file.');
+        if (!isset($json['project_id'])) throw new \Exception('Error: Credentials file missing project_id.');
+        if (!isset($json['client_email'])) throw new \Exception('Error: Credentials file missing client_email.');
+        if (!isset($json['private_key'])) throw new \Exception('Error: Credentials file missing private_key.');
+        if (!isset($json['type'])) throw new \Exception('Error: Credentials file missing type.');
+        if ($json['type'] !== 'service_account') throw new \Exception('Error: Credentials file type must be service_account.');
     }
 
     public function getResponse($promptData): GeminiResponse
