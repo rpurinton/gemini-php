171f544233069d964b7c6a06d812ac8a6ad9052c
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-18 01:01:55 +0000

    update

--- src/GeminiPrompt.php
+++ src/GeminiPrompt.php
@@ -2,12 +2,15 @@
 
 namespace RPurinton\GeminiPHP;
 
+use TikToken\Encoder;
+
 class GeminiPrompt
 {
-    private array $generationConfig;
-    private array $contents;
-    private array $safetySettings;
-    private array $tools;
+    private ?array $generationConfig;
+    private ?array $contents;
+    private ?array $safetySettings;
+    private ?array $tools;
+    private ?Encoder $encoder;
 
     public function __construct(array $dependencies)
     {
@@ -15,11 +18,17 @@ class GeminiPrompt
         $this->contents = $dependencies['contents'];
         $this->safetySettings = $dependencies['safety_settings'] ?? [];
         $this->tools = $dependencies['tools'] ?? [];
+        $this->encoder = new Encoder();
     }
 
     public function push(array $new_content): void
     {
         $this->contents[] = $new_content;
+    }
+
+    public function token_count($text)
+    {
+        return count($this->encoder->encode($text));
     }
 
     public function toJson(): string
