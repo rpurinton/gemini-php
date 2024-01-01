2b60c0699f2dbced7fc72bf1f76064a6a441438b
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-01 18:48:54 -0500

    Update GeminiClientTest.php

--- tests/GeminiClientTest.php
+++ tests/GeminiClientTest.php
@@ -7,17 +7,10 @@ use RPurinton\GeminiPHP\GeminiClient;
 
 class GeminiClientTest extends TestCase
 {
-    public function testValidateCredentials(): void
+    public function testInvalidCredentials(): void
     {
         $this->expectException(\Exception::class);
         $client = new GeminiClient(['credentialsPath' => 'invalid_path.json']);
-        $client->validateCredentials();
-    }
-
-    public function testGetResponse(): void
-    {
-        $this->expectException(\Exception::class);
-        $client = new GeminiClient(['credentialsPath' => 'valid_path.json', 'projectId' => 'project_id', 'regionName' => 'region', 'modelName' => 'model']);
-        $client->getResponse('{}');
+        $client->refreshAccessToken();
     }
 }
