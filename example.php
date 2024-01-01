8e972e5362d08cd33286a65491101a72293d2898
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-01 16:22:22 -0500

    Update example.php

--- example.php
+++ example.php
@@ -8,38 +8,43 @@ use RPurinton\GeminiPHP\{GeminiClient, GeminiPrompt};
 // https://cloud.google.com/vertex-ai/docs/generative-ai/model-reference/overview
 // https://cloud.google.com/vertex-ai/docs/generative-ai/model-reference/gemini#chat
 
-$client = new GeminiClient([
-    'projectId' => 'ai-project-123456', // Your Project ID
-    'regionName' => 'us-east4', // Google Cloud Region
-    'credentialsPath' => '/home/you/.google/ai-project-123456-7382b3944223.json', // Path to Service Account Credentials
-    'modelName' => 'gemini-pro', // AI Model to use gemini-pro / gemini-pro-vision
-]);
+try {
+    $client = new GeminiClient([
+        'projectId' => 'ai-project-123456', // Your Project ID
+        'regionName' => 'us-east4', // Google Cloud Region
+        'credentialsPath' => '/home/you/.google/ai-project-123456-7382b3944223.json', // Path to Service Account Credentials
+        'modelName' => 'gemini-pro', // AI Model to use gemini-pro / gemini-pro-vision
+    ]);
 
-$prompt = new GeminiPrompt([
-    'generation_config' => [ // Max values shown
-        'temperature' => 1.0,
-        'topP' => 1.0,
-        'topK' => 40,
-        'maxOutputTokens' => 2048,
-    ],
-    'contents' => [ // Must alternate user/assistant/user/assistant
-        [
-            'role' => 'user',
-            'parts' => ['text' => 'You are a helpful assistant.'],
+    $prompt = new GeminiPrompt([
+        'generation_config' => [ // Max values shown
+            'temperature' => 1.0,
+            'topP' => 1.0,
+            'topK' => 40,
+            'maxOutputTokens' => 2048,
         ],
-        [
-            'role' => 'assistant',
-            'parts' => ['text' => 'I am a helpful assistant!'],
+        'contents' => [ // Must alternate user/assistant/user/assistant
+            [
+                'role' => 'user',
+                'parts' => ['text' => 'You are a helpful assistant.'],
+            ],
+            [
+                'role' => 'assistant',
+                'parts' => ['text' => 'I am a helpful assistant!'],
+            ],
         ],
-    ],
-    'safety_settings' => [
-        [
-            'category' => 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
-            'threshold' => 'BLOCK_LOW_AND_ABOVE',
+        'safety_settings' => [
+            [
+                'category' => 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+                'threshold' => 'BLOCK_LOW_AND_ABOVE',
+            ],
         ],
-    ],
-    'tools' => [],
-]);
+        'tools' => [],
+    ]);
+} catch (\Exception $e) {
+    echo ('fatal> ' . $e->getMessage() . PHP_EOL);
+    exit(1);
+}
 
 $commands = function ($user_input) use ($prompt): bool {
     $command = strtolower($user_input);
