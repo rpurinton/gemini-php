17ab6b7ae94059e5fb498f01d52a4734eec7d3db
Russell Purinton <russell.purinton@icloud.com>
2024-01-02 01:40:58 +0000

    push

--- example.php
+++ example.php
@@ -53,7 +53,7 @@ $commands = function ($user_input) use ($prompt): bool {
         case 'quit':
             exit(0);
         case 'clear':
-            $prompt->setContent([]);
+            $prompt->resetContents();
             echo ('Prompt cleared.' . PHP_EOL);
             return true;
         case 'help':
