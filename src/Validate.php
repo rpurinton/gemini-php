4546a51d13f89c360ce30a679ab9c513f0e27da0
Robson Dantas <robson@simpleps.com>
2024-07-22 15:22:43 -0400

    fixing right gemini types for validation

--- src/Validate.php
+++ src/Validate.php
@@ -26,6 +26,10 @@ class Validate
     const VALID_MODELS = [
         'gemini-pro', // 32k token model (text + function calling)
         'gemini-pro-vision', // 16k multi-modal model (text + images + video + function calling)
+        'gemini-1.5-pro-001',
+        'gemini-1.5-flash-001',
+        'gemini-1.0-pro-vision-001',
+        'gemini-experimental',
     ];
 
     const VALID_CATEGORIES = [
